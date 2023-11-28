7975bfbc60d53c11f89cd43a937147527657a2b8
Brennan <brennanosunkwo14@gmail.com>
2023-11-28 02:00:55 -0500

    completed calculator-app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,75 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react'
 
 function App() {
+  //creating our state variables to be able to do our calculator operations
+  const [calculation, setCalculation] = useState("");
+  const [result, setResult]  = useState("");
+  const operators = ['/', '*', '+', '-', '.'];
+  const updateCalculations = value =>{
+          //limiting the number of operations, preventing from adding a lot of operations
+        if(operators.includes(value) && calculation=== '' ||
+            operators.includes(value) && operators.includes(calculation.slice(-1))
+        ){
+          return;
+        }
+        setCalculation(calculation + value);
+        if(!operators.includes(value)){
+          setResult(eval(calculation + value).toString());
+        }
+  }
+
+
+
+  //creating function to generated calculatoe buttons from 1-9
+  const createNumbers = () => {
+    const numbers = []; //creatong array of digits to store numbers
+    for (let i=1; i < 10; i++){
+        numbers.push(
+          <button key={i} onClick={() => updateCalculations(i.toString())}>{i}</button>
+        )
+    }
+    return numbers;
+  }
+
+  //Setting up the function for creating the equal button and converting the number to a String
+  const calc = () =>{
+    setCalculation(eval(calculation).toString());
+  }
+
+  //Setting up the function for creating the delete button after input
+  const deleteLastNumber = () =>{
+      if(calculation === ''){
+        return;
+      }
+      else{
+        const value = calculation.slice(0,-1);
+        setCalculation(value);
+      }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div className = "calculator">
+      <div className = "display" id="display">
+        { result ? <span>({result})</span>: ''}&nbsp; 
+        { calculation || "0" }
+      </div>
+      <div className = "operators">
+        <button id="divide" onClick={() => updateCalculations('/')}>/</button>
+       <button id="multiply" onClick={() => updateCalculations('*')}>*</button>
+        <button id="add" onClick={() => updateCalculations('+')}>+/</button>
+        <button id="subtract" onClick={() => updateCalculations('-')}>-</button>
+        <button id="clear" onClick={deleteLastNumber}>DEL</button>
+      </div>
+      <div className="numbers">
+        { createNumbers() }
+        <button onClick={() => updateCalculations('0')}>0</button>
+        <button id="decimal" onClick={() => updateCalculations('.')}>.</button>
+
+        <button id="equals" onClick={calc}>=</button>
+      </div>
+     </div>
     </div>
   );
 }
